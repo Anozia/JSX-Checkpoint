26a5c745403fdb78286993142412e59f94f86466
Kelechi Anozia <kelechianozia@gmail.com>
2022-04-03 08:46:29 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import "./imageInSrc.jpg";
+
+export const App = () => { <
+    div >
+        <
+        div style = {
+            { border: "solid 1px black", maxWidth: "100vw" } } >
+        <
+        h1 className = "title red" > Kelechi < /h1> <br / >
+        <
+        img src = "/imageInSrc.jpg"
+    alt = "img" / >
+        <
+        br / >
+        <
+        img src = "/imageInPublic.jpg"
+    alt = "img" / >
+        <
+        /div>{" "} <
+        video width = { 320 }
+    height = { 240 }
+    controls >
+        <
+        source src = "myVideo.mp4"
+    type = "video/mp4" / >
+        <
+        /video>{" "} <
+        /div>;
+};
+
+export default App;
