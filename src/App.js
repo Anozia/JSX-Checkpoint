ab3ad9a913752c2a4517fa8292bc729a25e96336
Kelechi Anozia <kelechianozia@gmail.com>
2022-04-04 11:36:54 +0100

    added changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import "./App.css";
+import Checkpoint from "./component/Checkpoint";
 import "./imageInSrc.jpg";
 
-export const App = () => { <
-    div >
-        <
-        div style = {
-            { border: "solid 1px black", maxWidth: "100vw" } } >
-        <
-        h1 className = "title red" > Kelechi < /h1> <br / >
-        <
-        img src = "/imageInSrc.jpg"
-    alt = "img" / >
-        <
-        br / >
-        <
-        img src = "/imageInPublic.jpg"
-    alt = "img" / >
-        <
-        /div>{" "} <
-        video width = { 320 }
-    height = { 240 }
-    controls >
-        <
-        source src = "myVideo.mp4"
-    type = "video/mp4" / >
-        <
-        /video>{" "} <
-        /div>;
+const App = () => {
+  return (
+    <div>
+      <Checkpoint />;
+    </div>
+  );
 };
 
 export default App;
--- src/component/Checkpoint.js
+++ src/component/Checkpoint.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Checkpoint = () => {
-  return <Checkpoint />;
+const Checkpoint = () => {
+  <div>
+    <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+      <h1 className="title red"> Kelechi </h1> <br />
+      <img src="/imageInSrc.jpg" alt="img" />
+      <br />
+      <img src="/imageInPublic.jpg" alt="img" />
+    </div>{" "}
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>{" "}
+  </div>;
 };
+
+export default Checkpoint;
